5017ba8786bcb43bb03a015d1838d77e595ef5d4
Luc Tielen <luc.tielen@gmail.com>
2016-12-24 10:07:27 +0100

    Add initial test harness

---
+++ test/ParserSpec.hs
@@ -0,0 +1,9 @@
+
+module ParserSpec ( spec ) where
+
+import Test.Tasty.Hspec
+
+spec :: Spec
+spec = describe "Parser" $ do
+    return ()
+
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,17 @@
+
+import Test.Tasty
+import Test.Tasty.Hspec as Hspec
+import qualified ParserSpec
+
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    unitTests <- Hspec.testSpec "Unit tests" mainSpec
+    defaultMain $ testGroup "Tests" [unitTests]
+
+
+mainSpec :: Spec
+mainSpec = do
+    describe "ParserSpec" ParserSpec.spec
+    -- describe "OtherSpec" OtherSpec.spec
+

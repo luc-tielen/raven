3a0d437c12bb6a09b1fc348ba835baaf7eb29a48
Luc Tielen <luc.tielen@gmail.com>
2016-12-28 23:14:40 +0100

    Implement Num instance for Number

--- src/Raven/Number.hs
+++ src/Raven/Number.hs
@@ -7,8 +7,9 @@ type Nominator = Int
 type Denominator = Int
 type RealPart = Double
 type ImagPart = Double
+-- TODO rename to avoid name clashes with builtin haskell types
 data Number = Integral Int
-            | Rational Nominator Denominator
+            | Rational Nominator Denominator  -- Assumes Denominator >= 0 (TODO enforce with type)
             | Real Double
             | Complex RealPart ImagPart
 
@@ -38,3 +39,58 @@ instance Eq Number where
   Real a == Complex r i = i == 0 && a == r
   Complex r1 i1 == Complex r2 i2 = r1 == r2 && i1 == i2
   a == b = b == a  -- fallback case, swaps arguments -> ends up in other cases
+
+
+instance Num Number where
+  Integral a + Integral b = Integral $ a + b
+  Integral a + Rational n d = Rational (n + a * d) d  -- TODO simplify fraction?
+  Integral a + Real b = Real $ (fromIntegral a) + b
+  Integral a + Complex r i = Complex ((fromIntegral a) + r) i
+  Rational n1 d1 + Rational n2 d2 = Rational (n1 * d2 + n2 * d1) (d1 * d2)  -- TODO simplify fraction?
+  Rational n d + Real a = Real $ (fromIntegral n / fromIntegral d) + a
+  Rational n d + Complex r i = Complex ((fromIntegral n / fromIntegral d) + r) i
+  Real a + Real b = Real $ a + b
+  Real a + Complex r i = Complex (a + r) i
+  Complex r1 i1 + Complex r2 i2 = Complex (r1 + r2) (i1 + i2)
+  a + b = b + a  -- fallback case, swaps arguments
+
+  Integral a - Integral b = Integral $ a - b
+  Integral a - Rational n d = Rational (a * d - n) d  -- TODO simplify fraction?
+  Integral a - Real b = Real $ (fromIntegral a) - b
+  Integral a - Complex r i = Complex ((fromIntegral a) - r) (negate i)
+  Rational n d - Integral a = Rational (n - a * d) d  -- TODO simplify fraction?
+  Rational n1 d1 - Rational n2 d2 = Rational (n1 * d2 - n2 * d1) (d1 * d2)  -- TODO simplify fraction?
+  Rational n d - Real a = Real $ (fromIntegral n / fromIntegral d) - a
+  Rational n d - Complex r i = Complex ((fromIntegral n / fromIntegral d) - r) (negate i)
+  Real b - Integral a = Real $ b -(fromIntegral a)
+  Real a - Rational n d = Real $ a - (fromIntegral n / fromIntegral d)
+  Real a - Real b = Real $ a - b
+  Real a - Complex r i = Complex (a - r) (negate i)
+  Complex r i - Integral a = Complex (r - (fromIntegral a)) i
+  Complex r i - Rational n d = Complex (r - (fromIntegral n / fromIntegral d)) i
+  Complex r i - Real a = Complex (r - a) i
+  Complex r1 i1 - Complex r2 i2 = Complex (r1 - r2) (i1 - i2)
+  
+  Integral a * Integral b = Integral $ a * b
+  Integral a * Rational n d = Rational (a * n) d  -- TODO simplify fraction?
+  Integral a * Real b = Real $ (fromIntegral a) * b
+  Integral a * Complex r i = Complex (a' * r) (a' * i) where a' = fromIntegral a
+  Rational n1 d1 * Rational n2 d2 = Rational (n1 * n2) (d1 * d2)  -- TODO simplify fraction?
+  Rational n d * Real a = Real $ a * fromIntegral n / fromIntegral d
+  Rational n d * Complex r i = Complex (f * r) (f * i) where f = fromIntegral n / fromIntegral d
+  Real a * Real b = Real $ a * b
+  Real a * Complex r i = Complex (a * r) (a * i)
+  Complex r1 i1 * Complex r2 i2 = Complex (r1 * r2 - i1 * i2) (r1 * i2 + i1 * r2)
+  a * b = b * a  -- fallback case, swaps arguments
+
+  abs (Integral a) = Integral $ abs a
+  abs (Rational n d) = Rational (abs n) d
+  abs (Real a) = Real $ abs a
+  abs (Complex _ _) = error "abs not supported for complex numbers!"
+
+  signum (Integral a) = Integral $ signum a
+  signum (Rational n _) = Integral $ signum n
+  signum (Real a) = Integral $ round $ signum a
+  signum (Complex _ _) = error "signum not supported for complex numbers!"
+
+  fromInteger = Integral . fromInteger
--- test/Raven/NumberSpec.hs
+++ test/Raven/NumberSpec.hs
@@ -3,6 +3,7 @@ module Raven.NumberSpec ( spec ) where
 
 import Test.Tasty.Hspec
 import Test.QuickCheck
+import Control.Exception
 import Raven.Number
 
 
@@ -56,6 +57,7 @@ spec = describe "Numerical tower behavior" $ do
         checkRatShow 0 1 "0/1"
         checkRatShow 1 1 "1/1"
         checkRatShow 2 6 "2/6"
+        checkRatShow (negate 2) 6 "-2/6"
         checkRatShow 88 113 "88/113"
         checkRealShow 0.1 "0.1"
         checkRealShow 1.0 "1.0"
@@ -67,3 +69,110 @@ spec = describe "Numerical tower behavior" $ do
         checkComplexShow 0 1 "1.0i"
         checkComplexShow 0 (negate 1) "-1.0i"
         checkComplexShow 1 0 "1.0+0.0i"
+
+    describe "Num" $ do
+      it "should be possible to perform addition with numbers" $ property $ do
+        \a b c n1 d1 n2 d2 r i -> d2 /= 0 && d1 /= 0 ==> do
+          Integral a + Integral b `shouldBe` Integral (a + b)
+          Integral a + Rational n1 d1 `shouldBe` Rational (a * d1) d1 + Rational n1 d1
+          Integral a + Real c `shouldBe` Real (fromIntegral a + c)
+          Integral a + Complex r i `shouldBe` Complex (fromIntegral a + r) i
+          Rational n1 d1 + Integral a `shouldBe` Integral a + Rational n1 d1
+          Rational n1 d1 + Rational n2 d2 `shouldBe` Rational (n1 * d2 + n2 * d1) (d1 * d2)
+          Rational n1 d1 + Real c `shouldBe` Real ((fromIntegral n1 / fromIntegral d1) + c)
+          Rational n1 d1 + Complex r i `shouldBe` Complex (r + (fromIntegral n1 / fromIntegral d1)) i
+          Real c + Integral a `shouldBe` Integral a + Real c
+          Real c + Rational n1 d1 `shouldBe` Rational n1 d1 + Real c
+          Real c + Real r `shouldBe` Real (c + r)
+          Real c + Complex r i `shouldBe` Complex (c + r) i
+          Complex r i + Integral a `shouldBe` Integral a + Complex r i
+          Complex r i + Rational n1 d1 `shouldBe` Rational n1 d1 + Complex r i
+          Complex r i + Real c `shouldBe` Real c + Complex r i
+          Complex r i + Complex c c `shouldBe` Complex (r + c) (i + c)
+
+      it "should be possible to perform substraction with numbers" $ property $ do
+        \a b c n1 d1 n2 d2 r i -> d2 /= 0 && d1 /= 0 ==> do
+          Integral a - Integral b `shouldBe` Integral (a - b)
+          Integral a - Rational n1 d1 `shouldBe` Rational (a * d1) d1 - Rational n1 d1
+          Integral a - Real c `shouldBe` Real (fromIntegral a - c)
+          Integral a - Complex r i `shouldBe` Complex (fromIntegral a - r) (negate i)
+          Rational n1 d1 - Integral a `shouldBe` Rational (n1 - a * d1) d1
+          Rational n1 d1 - Rational n2 d2 `shouldBe` Rational (n1 * d2 - n2 * d1) (d1 * d2)
+          Rational n1 d1 - Real c `shouldBe` Real ((fromIntegral n1 / fromIntegral d1) - c)
+          Rational n1 d1 - Complex r i `shouldBe` Complex ((fromIntegral n1 / fromIntegral d1) - r) (negate i)
+          Real c - Integral a `shouldBe` Real (c - fromIntegral a)
+          Real c - Rational n1 d1 `shouldBe` Real (c - (fromIntegral n1 / fromIntegral d1))
+          Real c - Real r `shouldBe` Real (c - r)
+          Real c - Complex r i `shouldBe` Complex (c - r) (negate i)
+          Complex r i - Integral a `shouldBe` Complex (r - fromIntegral a) i
+          Complex r i - Rational n1 d1 `shouldBe` Complex (r - fromIntegral n1 / fromIntegral d1) i
+          Complex r i - Real c `shouldBe` Complex (r - c) i
+          Complex r i - Complex c c `shouldBe` Complex (r - c) (i - c)
+
+      it "should be possible to perform multiplication with numbers" $ property $ do
+        \a b c n1 d1 n2 d2 r i -> d2 /= 0 && d1 /= 0 ==> do
+          Integral a * Integral b `shouldBe` Integral (a * b)
+          Integral a * Rational n1 d1 `shouldBe` Rational (a * d1) d1 * Rational n1 d1
+          Integral a * Real c `shouldBe` Real ((fromIntegral a) * c)
+          Integral a * Complex r i `shouldBe` Complex (fromIntegral a * r) (fromIntegral a * i)
+          Rational n1 d1 * Integral a `shouldBe` Integral a * Rational n1 d1
+          Rational n1 d1 * Rational n2 d2 `shouldBe` Rational (n1 * n2) (d1 * d2)
+          Rational n1 d1 * Real c `shouldBe` Real (c * fromIntegral n1 / fromIntegral d1)
+          Rational n1 d1 * Complex r i `shouldBe` Complex ((fromIntegral n1 / fromIntegral d1) * r) ((fromIntegral n1 / fromIntegral d1) * i)
+          Real c * Integral a `shouldBe` Integral a * Real c
+          Real c * Rational n1 d1 `shouldBe` Rational n1 d1 * Real c
+          Real c * Real r `shouldBe` Real (c * r)
+          Real c * Complex r i `shouldBe` Complex (c * r) (c * i)
+          Complex r i * Integral a `shouldBe` Integral a * Complex r i
+          Complex r i * Rational n1 d1 `shouldBe` Rational n1 d1 * Complex r i
+          Complex r i * Real c `shouldBe` Real c * Complex r i
+          Complex r i * Complex c c `shouldBe` Complex (r * c - i * c) (r * c + i * c)
+
+      it "should be possible to calculate abs value for a number (except complex numbers)" $ do
+        let checkIntAbs a b = abs (Integral a) `shouldBe` (Integral b)
+        let checkRatAbs a b = abs (Rational a 1) `shouldBe` (Rational b 1)
+        let checkRealAbs a b = abs (Real a) `shouldBe` (Real b)
+        checkIntAbs 0 0
+        checkIntAbs (negate 2) 2
+        checkIntAbs 2 2
+        checkRatAbs 0 0
+        checkRatAbs (negate 2) 2
+        checkRatAbs 2 2
+        checkRealAbs 0 0
+        checkRealAbs (negate 2) 2
+        checkRealAbs 2 2
+        
+      it "raises an error if calculating abs for a complex number" $ do
+        let checkAbs a b = evaluate (abs (Complex a b)) `shouldThrow` anyException
+        checkAbs 0 0
+        checkAbs 0 1
+        checkAbs 1 0
+        checkAbs 1 1
+
+      it "should be possible to get the sign of a number (except complex numbers)" $ do
+        let checkIntSign a b = signum (Integral a) `shouldBe` (Integral b)
+        let checkRatSign a b = signum (Rational a 1) `shouldBe` (Integral b)
+        let checkRealSign a b = signum (Real a) `shouldBe` (Integral b)
+        checkIntSign 0 0
+        checkIntSign (negate 2) (negate 1)
+        checkIntSign 2 1
+        checkRatSign 0 0
+        checkRatSign (negate 2) (negate 1)
+        checkRatSign 2 1
+        checkRealSign 0 0
+        checkRealSign (negate 2) (negate 1)
+        checkRealSign 2 1
+
+      it "raises an error if calculating the sign for a complex number" $ do
+        let checkSign a b = evaluate (signum (Complex a b)) `shouldThrow` anyException
+        checkSign 0 0
+        checkSign 0 1
+        checkSign 1 0
+        checkSign 1 1
+        
+      it "should be possible to convert an Integer to a Number" $ do
+        let checkFromInteger a b = ((fromInteger a) :: Number) `shouldBe` b
+        checkFromInteger 0 (Integral 0)
+        checkFromInteger 1 (Integral 1)
+        checkFromInteger 10 (Integral 10)
+        
